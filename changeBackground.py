1b0bacd08b9b8285d5d32fde4ad613cf87675d74
umw111 <utheriwagura@gmail.com>
2020-06-26 00:11:12 -0400

    APOD Background First Draft

---
+++ changeBackground.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import os
+import urllib.request
+import requests
+import ctypes
+
+# Check if there is currently an image saved, and delete it if it does exist
+if os.path.exists(r"C:\Users\uther\OneDrive\Documents\backgroundAPOD\APOD.jpg"):
+    os.remove(r"C:\Users\uther\OneDrive\Documents\backgroundAPOD\APOD.jpg")
+
+# Scrape img from page and store the URL to it's source, if there is one
+page = requests.get("https://apod.nasa.gov/apod/astropix.html")
+soup = BeautifulSoup(page.text, "html.parser")
+APOD = soup.find_all('img')
+
+# If there is an image today, set it as the background. 
+if len(APOD) != 0:
+    APODURL = "https://apod.nasa.gov/apod/" + str(APOD[0]["src"])
+    # Store the Image locally
+    image = open(r"C:\Users\uther\OneDrive\Documents\backgroundAPOD\APOD.jpg", "wb")
+    image.write(urllib.request.urlopen(APODURL).read())
+    # Set Image as background
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\Users\uther\OneDrive\Documents\backgroundAPOD\APOD.jpg", 0)
+# Otherwise, use the default
+else:
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\Users\uther\OneDrive\Documents\backgroundAPOD\default.jpg", 0)
